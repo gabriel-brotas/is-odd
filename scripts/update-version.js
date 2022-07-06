a1807ff303fe8c32c015882aedd37f320239f8b7
Gabriel Brotas <gabriel.brotas@wexinc.com>
2022-07-06 09:10:10 -0300

    fix: update script

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -3,6 +3,7 @@ const path = require('path')
 const axios = require('axios')
 const { execSync } = require("child_process")
 
+const SEMANTIC_VERSIONING_REGEX = /^([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$/
 
 async function getLastTag() {
     const {data} = await axios.default.get("https://api.github.com/repos/gabriel-brotas/is-odd/tags")
@@ -14,9 +15,18 @@ async function updatePackageVersion() {
 
     const packageJsonPath = path.join(__dirname, "../package.json")
     const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'))
+
+    if(!SEMANTIC_VERSIONING_REGEX.test(lastTag)) {
+        throw new Error("Tag doesn't follow Semantic versioning pattern")
+    }
+
+    if(packageJson.version == lastTag) return;
+
     packageJson.version = lastTag
 
     fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2))
+    
+    console.log("Publishing...")
 
     execSync('npm publish', {
         stdio: 'inherit'
